8bfb46ccbd0a5ee4da2b1fa748d48b1d4224a359
7Rebux <imrebux@gmail.com>
2021-05-07 15:29:28 +0200

    Now switches the GameState

--- src/main/kotlin/net/rebux/auraclassic/scheduler/IngameScheduler.kt
+++ src/main/kotlin/net/rebux/auraclassic/scheduler/IngameScheduler.kt
@@ -1,5 +1,7 @@
 package net.rebux.auraclassic.scheduler
 
+import net.rebux.auraclassic.utils.ConfigUtil
+import net.rebux.auraclassic.utils.GameState
 import net.rebux.auraclassic.AuraClassic as ac
 import org.bukkit.Bukkit
 
@@ -13,6 +15,7 @@ class IngameScheduler(override var delay: Long = 20L) : IScheduler
         {
             stop()
             // TODO end game stuff
+            ac.instance.gameState = GameState.POST_GAME
             PostGameScheduler().start()
             return
         }
@@ -20,14 +23,14 @@ class IngameScheduler(override var delay: Long = 20L) : IScheduler
         if (countdown == ac.instance.mainConfig.getInt("ingame_countdown") - ac.instance.mainConfig.getInt("tracker_countdown"))
         {
             // TODO give tracker
-            Bukkit.broadcastMessage(ac.instance.messagesConfig.getString("tracker_info"))
+            Bukkit.broadcastMessage(ConfigUtil.getMessage("tracker_info"))
         }
 
         if (countdown == 60)
-            Bukkit.broadcastMessage(ac.instance.messagesConfig.getString("ingame_end_info"))
+            Bukkit.broadcastMessage(ConfigUtil.getMessage("ingame_end_info"))
 
         if (listOf(90, 80, 70, 60, 50, 40, 30, 20, 15, 10, 5, 4, 3, 2, 1).contains(countdown))
-            Bukkit.broadcastMessage(ac.instance.messagesConfig.getString("ingame_end_timer").replace("{time}", countdown.toString()))
+            Bukkit.broadcastMessage(ConfigUtil.getMessage("ingame_end_timer").replace("{time}", countdown.toString()))
 
         --countdown
     }
