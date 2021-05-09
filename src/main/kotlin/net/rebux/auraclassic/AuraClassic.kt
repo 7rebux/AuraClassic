d0c416bb3ec46da0c099df4f20f70a0f91caad84
7Rebux <imrebux@gmail.com>
2021-05-09 23:13:11 +0200

    Created scheduler objects

--- src/main/kotlin/net/rebux/auraclassic/AuraClassic.kt
+++ src/main/kotlin/net/rebux/auraclassic/AuraClassic.kt
@@ -1,8 +1,10 @@
 package net.rebux.auraclassic
 
-import net.rebux.auraclassic.listeners.ConnectionListener
-import net.rebux.auraclassic.scheduler.WaitingScheduler
+import net.rebux.auraclassic.listeners.*
+import net.rebux.auraclassic.scheduler.*
+import net.rebux.auraclassic.utils.ConfigUtil
 import net.rebux.auraclassic.utils.GameState
+import net.rebux.auraclassic.utils.ItemUtil
 import org.bukkit.Bukkit
 import org.bukkit.configuration.file.YamlConfiguration
 import org.bukkit.entity.Player
@@ -16,9 +18,16 @@ class AuraClassic: JavaPlugin()
     val mainConfig: YamlConfiguration = YamlConfiguration.loadConfiguration(mainFile)
     val messagesConfig: YamlConfiguration = YamlConfiguration.loadConfiguration(messagesFile)
 
+    lateinit var waitingScheduler: WaitingScheduler
+    lateinit var preGameScheduler: PreGameScheduler
+    lateinit var protectionScheduler: ProtectionScheduler
+    lateinit var ingameScheduler: IngameScheduler
+    lateinit var postGameScheduler: PostGameScheduler
+
     val players = arrayListOf<Player>()
     val spectators = arrayListOf<Player>()
     val lastHitBy = mapOf<Player, Player>()
+
     lateinit var gameState: GameState
 
     companion object
@@ -30,11 +39,31 @@ class AuraClassic: JavaPlugin()
     {
         instance = this
 
+        // initialize schedulers
+        waitingScheduler = WaitingScheduler()
+        preGameScheduler = PreGameScheduler()
+        protectionScheduler = ProtectionScheduler()
+        ingameScheduler = IngameScheduler()
+        postGameScheduler = PostGameScheduler()
+
         // register event listeners
+        Bukkit.getPluginManager().registerEvents(MainListener(), this)
         Bukkit.getPluginManager().registerEvents(ConnectionListener(), this)
 
         gameState = GameState.PRE_GAME
 
-        WaitingScheduler().start()
+        waitingScheduler.start()
+    }
+
+    fun startGame()
+    {
+        players.addAll(Bukkit.getOnlinePlayers())
+        players.forEach { it.inventory.clear() }
+        players.forEach { player -> ItemUtil.getItems(player.uniqueId).forEach { player.inventory.addItem(it) } }
+        players.forEach { it.inventory.armorContents = ItemUtil.getArmor() }
+        protectionScheduler.start()
+        Bukkit.broadcastMessage(ConfigUtil.getMessage("protection_start"))
+
+        gameState = GameState.INGAME
     }
 }
