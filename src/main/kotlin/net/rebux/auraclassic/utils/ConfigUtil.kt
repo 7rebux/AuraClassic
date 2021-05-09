8d3f923e19025572b002f35a7755fb4e43ed336b
7Rebux <imrebux@gmail.com>
2021-05-09 23:13:26 +0200

    Now supports main config

--- src/main/kotlin/net/rebux/auraclassic/utils/ConfigUtil.kt
+++ src/main/kotlin/net/rebux/auraclassic/utils/ConfigUtil.kt
@@ -1,9 +1,13 @@
 package net.rebux.auraclassic.utils
 
-import net.rebux.auraclassic.AuraClassic
+import net.rebux.auraclassic.AuraClassic as ac
 import org.bukkit.ChatColor
 
 object ConfigUtil
 {
-    fun getMessage(name: String): String = ChatColor.translateAlternateColorCodes('&', AuraClassic.instance.messagesConfig.getString(name))
+    fun getMessage(name: String): String = ChatColor.translateAlternateColorCodes('&', ac.instance.messagesConfig.getString(name))
+
+    fun getString(name: String): String = ChatColor.translateAlternateColorCodes('&', ac.instance.mainConfig.getString(name))
+
+    fun getInt(name: String): Int = ac.instance.mainConfig.getInt(name)
 }
