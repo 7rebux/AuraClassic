cd9e402a70f2ae4e7b984132519678561b64a80d
7Rebux <imrebux@gmail.com>
2021-05-12 20:11:10 +0200

    Removed unused methods

--- src/main/kotlin/net/rebux/auraclassic/utils/GameUtil.kt
+++ src/main/kotlin/net/rebux/auraclassic/utils/GameUtil.kt
@@ -6,11 +6,6 @@ import org.bukkit.entity.Player
 
 object GameUtil
 {
-    fun startGame()
-    {
-
-    }
-
     fun addSpectator(player: Player)
     {
         AuraClassic.instance.spectators.add(player)
@@ -20,18 +15,5 @@ object GameUtil
     fun removeSpectator(player: Player)
     {
         AuraClassic.instance.spectators.remove(player)
-    }
-
-    fun checkForWin(): Player?
-    {
-        return if (AuraClassic.instance.players.size == 1)
-            AuraClassic.instance.players[0]
-        else
-            null
-    }
-
-    fun showWinner(player: Player)
-    {
-        Bukkit.broadcastMessage(ConfigUtil.getMessage("win").replace("{player}", player.name))
     }
 }
