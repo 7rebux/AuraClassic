b81bf729ee2bc2b4c2f21ee956c72cf12d09a310
7Rebux <imrebux@gmail.com>
2021-06-09 08:52:23 +0200

    Added location properties

--- src/main/kotlin/net/rebux/auraclassic/utils/ConfigUtil.kt
+++ src/main/kotlin/net/rebux/auraclassic/utils/ConfigUtil.kt
@@ -2,6 +2,8 @@ package net.rebux.auraclassic.utils
 
 import net.rebux.auraclassic.AuraClassic as ac
 import org.bukkit.ChatColor
+import org.bukkit.Location
+import org.bukkit.World
 
 object ConfigUtil {
     fun getMessage(name: String): String = ChatColor.translateAlternateColorCodes('&', ac.instance.messagesConfig.getString(name))
@@ -11,4 +13,16 @@ object ConfigUtil {
     fun getInt(name: String): Int = ac.instance.mainConfig.getInt(name)
 
     fun getBoolean(name: String): Boolean = ac.instance.mainConfig.getBoolean(name)
+
+    fun getLocation(name: String, world: World): Location {
+        val section = ac.instance.mainConfig.getConfigurationSection(name)
+
+        return Location(world,
+            section.getDouble("x"),
+            section.getDouble("y"),
+            section.getDouble("z"),
+            section.getDouble("yaw").toFloat(),
+            section.getDouble("pitch").toFloat()
+        )
+    }
 }
